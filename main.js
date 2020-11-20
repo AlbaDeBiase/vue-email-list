8e5dc8c5cbaae4647a69d2bb838d578b4db38293
Alba De biase <debiasealba@gmail.com>
2020-11-20 16:16:26 +0100

    stampo una mail

---
+++ main.js
@@ -0,0 +1,29 @@
+// generare una lista di 10 indirizzi email
+// e stamparle in pagina con vue.
+// Dopo aver recuperato correttamente 10 indirizzi email
+//  e averli stampati in pagina in modo "grezzo",
+//   date sfogo alla vostra creatività per rendere più accattivante
+//    la grafica della pagina
+
+
+
+var app = new Vue({
+
+    el: '#container',
+    data: {
+        mail:false
+
+    }, // chiudo data
+
+    mounted(){
+        const self=this;
+        axios
+            .get('https:flynn.boolean.careers/exercises/api/random/mail')
+            .then(function(risposta){
+            self.mail=risposta.data.response;
+        });
+
+    }
+
+
+}); // chiudo Vue
